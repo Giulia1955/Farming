c0c6eb7fe238990029d58b558b837d3ed9711df7
Eduardo Bizzi <eduardobizzi15@gmail.com>
2025-06-16 20:16:02 -0300

    Delete POO1 - Trabalho Final directory

--- POO1 - Trabalho Final/Farming-main/TrabalhoFinalPOO/src/Fazenda/Itens/La.java
+++
@@ -1,9 +0,0 @@
-package Fazenda.Itens;
-import Fazenda.Animais.Animal;
-import java.io.Serializable;
-
-public class La extends Item implements Serializable {
-    public La() {
-        super(10, "Já em forma de novelo", "Lã");
-    }
-}
--- POO1 - Trabalho Final/Farming-main/TrabalhoFinalPOO/src/Fazenda/Itens/sementeTrigo.java
+++
@@ -1,9 +0,0 @@
-package Fazenda.Itens;
-import Fazenda.Plantas.Planta;
-import java.io.Serializable;
-
-public class sementeTrigo extends Item implements Serializable {
-    public sementeTrigo(){
-        super (4, "sementes de trigo", "Semente Trigo");
-    }
-}
