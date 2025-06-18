e4ff0bc2ce19da202bf14d55e7bb5e12e11923b9
Giulia1955 <giuliamezaroba@gmail.com>
2025-06-18 16:33:00 -0300

    Delete TrabalhoFinalPOO directory

--- TrabalhoFinalPOO/src/Fazenda/Itens/Ovo.java
+++
@@ -1,8 +0,0 @@
-package Fazenda.Itens;
-import java.io.Serializable;
-
-public class Ovo extends Item implements Serializable {
-    public Ovo(){
-        super (4, "uma unidade de um único ovo", "Ovo");
-    }
-}
--- TrabalhoFinalPOO/src/Fazenda/Itens/filhoteOvelha.java
+++
@@ -1,8 +0,0 @@
-package Fazenda.Itens;
-import java.io.Serializable;
-
-public class filhoteOvelha extends Item implements Serializable {
-    public filhoteOvelha(){
-        super(40, "Uma linda novilha", "Filhote de ovelha");
-    }
-}
